a451d8b4e5f003d362aa30fcc80704cf4cca8f18
Aryan Thakre <60684679+aryanthakre@users.noreply.github.com>
2020-09-30 14:09:59 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+
+$(".roll").click(function(){
+var randomnumber1 = Math.floor(Math.random() * 6) + 1; 
+var photo = "images/dice" + randomnumber1 + ".png";
+var randomnumber2 = Math.floor(Math.random() * 6) + 1;
+var photo2 = "images/dice" + randomnumber2 + ".png";
+document.querySelector(".img1").setAttribute("src",photo);
+document.querySelector(".img2").setAttribute("src", photo2);
+
+
+if(randomnumber1 == randomnumber2){
+
+    document.querySelector(".heading").textContent = "Draw";
+    $(".heading").css("color","yellow");
+}else if ( randomnumber1 > randomnumber2) {
+
+
+    document.querySelector(".heading").textContent = "Player 1 Wins!";
+        $(".heading").css("color", "red");
+
+
+    
+
+
+}else if (randomnumber2 > randomnumber1){
+    document.querySelector(".heading").textContent = "Player 2 Wins!";
+        $(".heading").css("color", "blue");
+
+}else{
+   //Do Nothing 
+}
+});
